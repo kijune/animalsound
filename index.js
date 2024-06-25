4d0a255fef22ae7617e3ef97716b3d5c5d19bd5f
kijune <42115942+kijune@users.noreply.github.com>
2024-06-26 01:20:50 +0900

    6666

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express()
 
 const port = 3000
 
-app.use(cors({ origin: '*' }));
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
